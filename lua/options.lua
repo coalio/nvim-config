78289a96d7cb614eb8c108a2eb837dec7fa6cc49
Coal <coalio.gg@gmail.com>
2026-05-14 10:39:30 -0600

    chore: save local nvim options

--- lua/options.lua
+++ lua/options.lua
@@ -3,5 +3,7 @@ require "nvchad.options"
 -- Disables 'unnamedplus' so y, d, c, p use Neovim's internal registers.
 -- System clipboard will ONLY be used when you explicitly press Ctrl+C or Ctrl+V.
 vim.opt.clipboard = ""
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.o.guifont = "Hack Nerd Font Mono:h12"
