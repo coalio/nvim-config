34064dbc8c72966b4bbf12377548e69936cdd4ad
Coal <coalio.gg@gmail.com>
2026-08-10 10:51:20 -0600

    feat(markdown): add rendered Markdown support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -83,10 +83,22 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
+        "markdown",
+        "markdown_inline",
         "python",
       })
       return opts
     end,
+  },
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
   },
 
   -- My stuff goes here
